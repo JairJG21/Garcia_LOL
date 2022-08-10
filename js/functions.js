c987565b8fa22bbffad8344eb85ce73fa105670c
bl4ck-00 <75991247+bl4ck-00@users.noreply.github.com>
2022-08-10 14:41:04 -0500

    Full Responsive

--- js/functions.js
+++ js/functions.js
@@ -1,37 +1,36 @@
 AOS.init();
 
     function abrirMenu() {
-
-      $('.navbar ul').css(
-        {
-          "display": "initial",
-          "position": "absolute",
-          "right": "0",
-          "top": "0",
-          "overflow": "hidden",
-          "background-color": "rgb(0, 0, 0)",
-          "width": "100%",
-          "animation-name": "abrirMenuAnimacion",
-          "animation-duration": "1s",
-          "border-bottom-left-radius": "20px",
-          "border-bottom-right-radius": "20px",
-          "box-shadow": "0 5px 15px rgb(255, 255 , 255)"
-        });
-
-        $('.btn_cerrar_menu').css(
-        {
-          "color": "rgb(255, 255, 255)",
-          "position": "absolute",
-          "top": "10px",
-          "right": "20px"
-        });
-
-        $('.navbar ul li').css(
-        {
-          "margin": "0 0 0 0",
-          "margin": "5px 0 0 0",
-          "padding": "10px 0 10px 20px"
-        });
+        $('.navbar ul').css(
+          {
+            "display": "initial",
+            "position": "absolute",
+            "right": "0",
+            "top": "0",
+            "overflow": "hidden",
+            "background-color": "rgb(0, 0, 0)",
+            "width": "100%",
+            "animation-name": "abrirMenuAnimacion",
+            "animation-duration": "1s",
+            "border-bottom-left-radius": "20px",
+            "border-bottom-right-radius": "20px",
+            "box-shadow": "0 5px 15px rgb(255, 255 , 255)"
+          });
+  
+          $('.btn_cerrar_menu').css(
+          {
+            "color": "rgb(255, 255, 255)",
+            "position": "absolute",
+            "top": "10px",
+            "right": "20px"
+          });
+  
+          $('.navbar ul li').css(
+          {
+            "margin": "0 0 0 0",
+            "margin": "5px 0 0 0",
+            "padding": "10px 0 10px 20px"
+          });
     }
 
     function cerrarMenu() {
