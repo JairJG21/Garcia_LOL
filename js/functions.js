f49276cfb66904efebf09cc7c2b41c98ea7950db
JairJG21 <gjair4267@gmail.com>
2022-09-07 15:56:12 -0500

    pruebas

--- js/functions.js
+++ js/functions.js
@@ -1,65 +1,36 @@
-    AOS.init();
 
-    function abrirMenu() {
-        $('.navbar ul').css(
-          {
-            "display": "initial",
-            "position": "absolute",
-            "right": "0",
-            "top": "0",
-            "overflow": "hidden",
-            "background-color": "rgb(0, 0, 0)",
-            "width": "100%",
-            "animation-name": "abrirMenuAnimacion",
-            "animation-duration": "1s",
-            "border-bottom-left-radius": "20px",
-            "border-bottom-right-radius": "20px",
-            "box-shadow": "0 5px 15px rgb(255, 255 , 255)"
-          });
+$(document).ready(function () {
+  AOS.init({
+    duration: 1200,
+  })
   
-          $('.btn_cerrar_menu').css(
-          {
-            "color": "rgb(255, 255, 255)",
-            "position": "absolute",
-            "top": "10px",
-            "right": "20px"
-          });
+  // Fondo
+  var instance = new vidbg(
+    ".background_0_index",
+    {
+      mp4: "https://assets.mixkit.co/videos/preview/mixkit-hands-of-a-man-playing-on-a-computer-43527-large.mp4",
+      overlay: true,
+      overlayColor: "#000",
+      overlayAlpha: 0.3
+    }
+    // {
+    //   autoplay: true,
+    //   controls: false,
+    //   loop: true,
+    //   muted: true,
+    //   playsInline: true,
+    // }
+  );
+  instance.playVideo();
+
+  var height_total = $(window).height();
+
+      $('.contenedor_principal').height(height_total);
+
+      var height_img = height_total - 95;
+      console.log(height_img);
+      console.log(height_total);
+      $('.heigth_img').height(height_img);
   
-          $('.navbar ul li').css(
-          {
-            "margin": "0 0 0 0",
-            "margin": "5px 0 0 0",
-            "padding": "10px 0 10px 20px"
-          });
-    }
-
-    function cerrarMenu() {
-
-      $('.navbar ul').css(
-        {
-          "display": "",
-          "position": "",
-          "right": "",
-          "top": "",
-          "overflow": "",
-          "background-color": "",
-          "width": "",
-          "animation-name": "",
-          "animation-duration": "",
-          "border-bottom-left-radius": "",
-          "border-bottom-right-radius": "",
-          "box-shadow": ""
-        });
-
-        $('.btn_cerrar_menu').css(
-        {
-          "color": ""
-        });
-
-        $('.navbar ul li').css(
-        {
-          "margin": "",
-          "margin": "0 20px 0 0",
-          "padding": ""
-        });
-    }
+  });
+    
