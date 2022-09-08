6d706845570c495234fbbe47aa8f58fe48853a0c
JairJG21 <gjair4267@gmail.com>
2022-09-07 22:31:16 -0500

    Version_3.0

--- js/functions.js
+++ js/functions.js
@@ -1,36 +1,22 @@
-
 $(document).ready(function () {
   AOS.init({
     duration: 1200,
-  })
-  
-  // Fondo
-  var instance = new vidbg(
-    ".background_0_index",
-    {
-      mp4: "https://assets.mixkit.co/videos/preview/mixkit-hands-of-a-man-playing-on-a-computer-43527-large.mp4",
-      overlay: true,
-      overlayColor: "#000",
-      overlayAlpha: 0.3
-    }
-    // {
-    //   autoplay: true,
-    //   controls: false,
-    //   loop: true,
-    //   muted: true,
-    //   playsInline: true,
-    // }
-  );
-  instance.playVideo();
+  });
 
+  reacomodarVideo();
+
+});
+
+$(window).resize(function () {
+  reacomodarVideo();
+});
+
+function reacomodarVideo() {
   var height_total = $(window).height();
 
-      $('.contenedor_principal').height(height_total);
-
-      var height_img = height_total - 95;
-      console.log(height_img);
-      console.log(height_total);
-      $('.heigth_img').height(height_img);
-  
-  });
-    
+  $('.contenedor_principal').height(height_total);
+  var height_img = height_total - 95;
+  console.log(height_img);
+  console.log(height_total);
+  $('.heigth_img').height(height_img);
+}
